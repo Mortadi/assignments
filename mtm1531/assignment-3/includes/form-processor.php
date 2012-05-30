d8a721eb7694b3a27f5269808822f1019d8310c9
Mortadi <mortadi1330@yahoo.com>
2012-05-30 17:24:48 -0400

    final form .. before testing

---
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -0,0 +1,47 @@
+<?php
+$errors = array();
+$completed = false;
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$notes = filter_input(INPUT_POST,'notes', FILTER_SANITIZE_STRING);
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW);
+$acceptterms = filter_input(INPUT_POST, 'acceptterms', FILTER_SANITIZE_STRING);
+$prel = filter_input(INPUT_POST, 'prel', FILTER_SANITIZE_STRING);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	if (empty($name)) {
+		$errors['name'] = true;
+	}
+	
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = true;
+	}
+	
+    if (strlen($username) < 1 || strlen ($username) > 25) {
+		$errors['username'] = true;
+	}
+	
+	if (strlen($notes)) { 
+		$errors['notes'] = true;
+	}
+	
+	if (empty($password)) {
+		$errors['password'] = true;
+	}
+	
+	if (!isset($_POST['acceptterms'])) {
+		$errors['acceptterms'] = true;
+	}
+	
+	if (!in_array($prel, array('en', 'fr', 'sp'))) {
+		$errors['prel'] = true;
+	}
+
+    if (empty($errors)) {
+     $completed = true;
+     mail($email, 'Thank you for your feedback and using our Registration Form', "From: Mohamed <elmo0008@algonquincollege.com>\r\n");
+
+}
+}
--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -1,5 +1,6 @@
 <?php
-?>
+require_once "includes/form-processor.php"
+?> 
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -8,38 +9,62 @@
 <link href="css/style.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
 </head>
+<body>
+<?php if ($completed) : ?>
+<strong class="thx">Thank you for using our registration form! <a href="http://imm.edumedia.ca/elmo0008/mtm1531/assignment-3/">Back to form</a> </strong>
+<?php else : ?>
+
 <h1>Registration Form Validation</h1>
 <form method="post" action="index.php">
 
 <div>
-<label for="name">Name</label>
-<input type="text" id="name" name="name">
+<label for="name">Name<?php if (isset($errors['name'])) : ?> <strong>is required</strong><?php endif; ?></label>
+<input type="text" id="name" name="name" value="<?php echo $name; ?>" required>
 </div>
 <div>
-<label for="email">E-mail</label>
-<input type="email" id="email" name="email">
+<label for="email">E-mail<?php if (isset($errors['email'])) : ?> <strong>Valid E-mail Address</strong><?php endif; ?></label>
+<input type="email" id="email" name="email" value="<?php echo $email; ?>" required>
 </div>
 <div>
-<label for="username">Username</label>
-<input type="text" id="username" name="username">
+<label for="username">Username<?php if (isset($errors['username'])) : ?> <strong>max length 25 characters</strong><?php endif; ?></label>
+<input type="text" id="username" name="username" value="<?php echo $username; ?>" required>
 </div>
 <div>
-<label for="password">Password</label>
-<input type="password" id="password" name="password">
+<label for="password">Password<?php if (isset($errors['password'])) : ?> <strong>is required</strong><?php endif; ?></label>
+<input type="password" id="password" name="password" value="<?php echo $password; ?>" required>
 </div>
 <div>
-<label for="preferredlang">Preferred Language</label>
-<input type="radio" id="prel" name="prel">
+<fieldset class="lang">
+<legend>Preferred Language</legend>
+<?php if (isset($errors['prel'])) : ?><strong>Select Language:</strong><?php endif; ?>
+<input type="radio" id="english" name="prel" value="en"<?php if ($prel == 'en') {
+echo ' checked'; }
+?>>
+<label for="english">English</label>
+<input type="radio" id="french" name="prel" value="fr"<?php if ($prel == 'fr') {
+echo 'checked';
+}?>>
+<label for="french">French</label>
+<input type="radio" id="spanish" name="prel" value="sp"<?php if ($prel == 'sp') {
+echo 'checked';
+}?>>
+<label for="spanish">Spanish</label>
+</fieldset>
+
+</div>
+
+<div class="notes">
+<label for="notes">Notes<?php if (isset($errors['notes'])) : ?><?php endif; ?></label>
+<textarea id="notes" name="notes"><?php echo $notes; ?></textarea>
 </div>
 <div>
-<label for="notes">Notes</label>
-<input type="textarea" id="notes" name="notes">
-</div>
-<div>
-<label for="acceptterms">Accept Terms</label>
-<input type="checkbox" id="acceptterms" name="acceptterms">
+<label for="acceptterms">Accept Terms<?php if (isset($errors['acceptterms'])) : ?><?php endif; ?></label>
+<input type="checkbox" id="acceptterms" name="acceptterms" value="<?php echo $acceptterms; ?>" required> 
+
+
+<button type="submit">Send</button>
 </div>
 </form>
-<body>
+<?php endif; ?>
 </body>
 </html>
