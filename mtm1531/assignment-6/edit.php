ad4249d20ea7b8d6a79f159ecd511906cf101975
Mortadi <mortadi1330@yahoo.com>
2012-06-20 14:48:21 -0400

    Made array to my db

--- mtm1531/assignment-6/edit.php
+++ mtm1531/assignment-6/edit.php
@@ -1,4 +1,70 @@
-<!DOCTYPE HTML>
+<?php
+
+require_once 'includes/db.php';
+
+	$errors = array();
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
+$genre = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_STRING);
+$directed_by = filter_input(INPUT_POST, 'directed_by', FILTER_SANITIZE_STRING);
+$release_date = filter_input(INPUT_POST, 'release_date', FILTER_SANITIZE_INT);
+$genre = filter_input(INPUT_POST, 'starring', FILTER_SANITIZE_STRING);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if (strlen($title) < 1 || strlen($title) > 60) {
+		$errors['title'] = true;
+	}
+	
+	if (strlen($genre) < 1 || strlen($genre) > 100) {
+		$errors['genre'] = true;
+	}
+	if (strlen($directed_by) < 1 || strlen($directed_by) > 40) {
+		$errors['directed_by'] = true;
+	}
+	if (strlen($release_date) < 1 || strlen($release_date) > ) {
+		$errors['release_date'] = true;
+	}
+	if (strlen($starring) < 1 || strlen($starring) > 256) {
+		$errors['starring'] = true;
+	}
+	
+	if (empty($errors)){
+		require_once 'includes/db.php';
+		
+		$sql = $db->prepare('
+			UPDATE list_of_movies
+			SET title = :title, genre = :genre, directed_by = :directed_by, release_date = :release_date, starring = :starring
+			WHERE id = :id
+		');
+		$sql->bindValue(':id', $id, PDO::PARAM_INT);
+		$sql->bindValue(':title', $title, PDO::PARAM_STR);
+		$sql->bindValue(':genre', $genre, PDO::PARAM_STR);
+		$sql->bindValue(':directed_by', $directed_by, PDO::PARAM_STR);
+		$sql->bindValue(':release_date', $release_date, PDO::PARAM_INT);
+		$sql->bindValue(':starring', $starring, PDO::PARAM_STR);
+		$sql->execute();
+		
+		header('Location: index.php');
+		exit;
+	}
+} else {
+	
+	$sql = $db->prepare('
+		SELECT title, genre, directed_by, release_date, starring
+		FROM list_of_movies
+		WHERE id = :id 
+	');
+	$sql->bindValue(':id', $id, PDO::PARAM_INT);
+	$sql->execute();
+	$results = $sql->fetch();
+	
+	$title = $results['title'];
+	$genre = $results['genre'];
+	$directed_by = $results['directed_by'];
+	$release_date = $results['release_date'];
+	$starring = $results['starring'];	
+}
+?><!DOCTYPE HTML>
 <html>
     <head>
         <meta charset="utf-8">
--- mtm1531/assignment-6/single.php
+++ mtm1531/assignment-6/single.php
@@ -43,6 +43,7 @@ $results = $sql->fetch();
 
 </dl>
 <a href="delete.php?id=<?php echo $id; ?>">Delete</a>
+<a href="edit.php?id=<?php echo $id; ?>">Edit</a>
 </body>
 </html>
 
