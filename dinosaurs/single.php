dbffc15f2b79590185201d5ca3feab2ce55a3f98
Mortadi <mortadi1330@yahoo.com>
2012-06-20 13:55:34 -0400

    Adding users and sign-in files

--- dinosaurs/single.php
+++ dinosaurs/single.php
@@ -33,5 +33,6 @@ $results = $sql->fetch();
 <dd><?php echo $results['in_jurassic_park']; ?></dd>
 </dl>
 <a href="delete.php?id=<?php echo $id; ?>">Delete</a>
+<a href="edit.php?id=<?php echo $id; ?>">Edit</a>
 </body>
 </html>
