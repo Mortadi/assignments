eaed867103a5980e87a101b0d21639107eba9174
Mortadi <mortadi1330@yahoo.com>
2012-07-13 13:07:23 -0400

    Added the insert functions to add a movie to my movies list

--- mtm1531/assignment-6/index.php
+++ mtm1531/assignment-6/index.php
@@ -7,7 +7,50 @@ ORDER BY title ASC
 ');
 
 $resultes = $sql->fetchAll();
+$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
+$genre = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_STRING);
+$directed_by = filter_input(INPUT_POST, 'directed_by', FILTER_SANITIZE_STRING);
+$starring = filter_input(INPUT_POST, 'starring', FILTER_SANITIZE_STRING);
+$release_date = filter_input(INPUT_POST, 'release_date', FILTER_SANITIZE_STRING);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if (strlen($title) < 1 || strlen($title) > 60) {
+		$errors['title'] = true;
+	}
+	
+	if (strlen($genre) < 1 || strlen($genre) > 100) {
+		$errors['genre'] = true;
+	}
+	if (strlen($directed_by) < 1 || strlen($directed_by) > 40) {
+		$errors['directed_by'] = true;
+	}
+	if (strlen($release_date) < 1 || strlen($release_date) > 25) {
+$errors['release_date'] = true;
+}
+	if (strlen($starring) < 1 || strlen($starring) > 256) {
+		$errors['starring'] = true;
+	}
+	
+	if (empty($errors)){
+		require_once 'includes/db.php';
+		
+		$sql = $db->prepare('
+			UPDATE list_of_movies
+			SET title = :title, genre = :genre, directed_by = :directed_by, release_date = :release_date, starring = :starring
+			WHERE id = :id
+		');
+		$sql->bindValue(':id', $id, PDO::PARAM_INT);
+		$sql->bindValue(':title', $title, PDO::PARAM_STR);
+		$sql->bindValue(':genre', $genre, PDO::PARAM_STR);
+		$sql->bindValue(':directed_by', $directed_by, PDO::PARAM_STR);
+		$sql->bindValue(':release_date', $release_date, PDO::PARAM_STR);
+		$sql->bindValue(':starring', $starring, PDO::PARAM_STR);
+		$sql->execute();
+		
+		header('Location: index.php');
+		exit;
+	}
+}
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -19,6 +62,9 @@ $resultes = $sql->fetchAll();
 </head>
 
 <body>
+
+
+
 <h1>'List of July movies 2012'</h1>
 <?php foreach ($resultes as $movie) : ?>
 
@@ -36,6 +82,46 @@ $resultes = $sql->fetchAll();
 
 </dl>
 <?php endforeach; ?>
+
+<h1>Add a Movie</h1>        
+        <form id="movie" action="index.php" method="post">
+        
+        	<label for="title">Title
+            	<?php if (isset($errors['title'])) : ?>
+                <strong class="error">is required.</strong>
+                <?php endif; ?>
+            </label>
+        	<input type="text" name="title" id="title" required value="<?php echo $title; ?>">
+        	
+        	<label for="genre">Genre
+            	<?php if (isset($errors['genre'])) : ?>
+                <strong class="error">is required.</strong>
+                <?php endif; ?>
+            </label>
+            <input type="text" name="genre" id="genre" required value="<?php echo $genre; ?>">
+        	
+        	<label for="directed_by">Directed By
+            	<?php if (isset($errors['directed_by'])) : ?>
+                <strong class="error">is required.</strong>
+                <?php endif; ?>
+            </label>
+        	<input type="text" name="directed_by" id="directed_by" required value="<?php echo $directed_by; ?>">
+        	
+        	<label for="genre">Starring
+            	<?php if (isset($errors['starring'])) : ?>
+                <strong class="error">is required.</strong>
+                <?php endif; ?>
+            </label>
+        	 <input type="text" name="starring" id="starring" required value="<?php echo $starring; ?>">
+        	<label for="release_date">Release Date
+            	<?php if (isset($errors['release_date'])) : ?>
+                <strong class="error">is required.</strong>
+                <?php endif; ?>
+            </label>
+        	<input type="text" name="release_date" id="release_date" required value="<?php echo $release_date; ?>" placeholder="yyyy-mm-dd">
+            
+            <button type="submit">Add</button>
+        </form>
 
 
 </body>
